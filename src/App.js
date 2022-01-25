258a0fd370377481187864603ca33735d32362a3
sylee93 <sylee050864@gmail.com>
2022-01-25 22:42:01 +0900

    caver.js(read)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import logo from './logo.svg';
 import Caver from 'caver-js';
 import './App.css';
 
-const COUNT_COUNTRACT_ADDRESS = '0x06248e146BDd0Fc68685f643E5570dcB0b613EEa';
+const COUNT_COUNTRACT_ADDRESS = '0x6533fd0c087CB621B9AC3C70298628130ef6EC85';
 const ACCESS_KEY_ID = 'KASKBY490VI08E20Z1JVOLGN';
 const SECRET_KEY_ID ='q68n5eUS0iPDXTNqnT0OBUCihWJfiWOWDS3xhK1o';
+const COUNT_ABI = '[ { "constant": true, "inputs": [], "name": "count", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getBlockNumber", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_count", "type": "uint256" } ], "name": "setCount", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" } ]';
 const CHAIN_ID = '1001'; // MAINNET 8217 / TESTNET 1001
 
 const option = {
   headers: [
     {
-      name: "Authorrization",
+      name: "Authorization",
       value: "Basic " + Buffer.from(ACCESS_KEY_ID + ":" + SECRET_KEY_ID).toString("base64")
     },
     {name: "x-chain-id", value: CHAIN_ID}
@@ -18,18 +19,32 @@ const option = {
 }
 
 const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", option));
+const CountContract = new caver.contract(JSON.parse(COUNT_ABI), COUNT_COUNTRACT_ADDRESS);
+const readCount =() =>{
+  const _count = CountContract.methods.count().call();
+  console.log(_count);
+}
 
+const getBalance = (address) => {
+  return caver.rpc.klay.getBalance(address).then((response) => { // then((response) => ) 응답이 올 경우 실행
+    const balance = caver.utils.convertFromPeb(caver.utils.hexToNumberString(response));
+    console.log(`BALANCE: ${balance}`);
+    return balance;
+  })
+}
 // 1 Smart contract 배포 주소 파악(가져오기)
 // 2 caver.js 이용해서 스마트 컨트랙트 연동하기
 // 3 가져온 스마트 컨트랙트 실행 결과 웹에 표현하기
 
 function App() {
+  readCount();
+  getBalance('0x8724a8f3a71d65edcb3a5bfff7913eceecedbb7c');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          good <code>src/App.js</code> and save to reload.
+          GOOD <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -38,7 +53,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>r
+        </a>
       </header>
     </div>
   );
