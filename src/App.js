f7a262a4381e49d0891c26ffb41392626bc1b002
sylee93 <sylee050864@gmail.com>
2022-02-13 00:12:36 +0900

    BApp 기능 보완하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import QRcode from "qrcode.react";
 import {fetchCardsOf, getBalance} from './api/UseCaver.js';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import './market.css';
 import * as KlipAPI from "./api/UseKlip";
-import { Alert, Card, Container, Nav, Form, Button } from "react-bootstrap";
+import { Alert, Card, Container, Nav, Form, Button, Modal, Row, Col } from "react-bootstrap";
 import { MARKET_CONTRACT_ADDRESS } from './constants';
 
 // 1 Smart contract 배포 주소 파악(가져오기)
@@ -23,11 +23,6 @@ const DEFAULT_QR_CODE = "DEFAULT";
 const DEFAULT_ADDRESS = "0x00000000000000000000000000000";
 
 function App() {
-  // state data
-
-  // gloval data
-  // address
-  // nft
   const [nfts, setNFTs] = useState([]); // {id:'101', uri: ''}
   const [myBalance, setMyBalance] = useState("0");
   //const [myAddress, setMyAddress] = useState(DEFAULT_ADDRESS);
@@ -35,27 +30,36 @@ function App() {
 
   // UI
   const [qrvalue, setQrvalue] = useState(DEFAULT_QR_CODE);
-  const [tab, setTab] = useState("MINT") // MARKET, MINT, WALLET
+  const [tab, setTab] = useState("MARKET") // MARKET, MINT, WALLET
   const [mintImageUrl, setMintImageUrl] = useState("");
-  // tab
-  // mintInput
-
-  // Modal
   
-  // fetchMarketNFTs
+  // Modal
+  const [showModal, setShowModal] = useState(false);
+  const [modalProps, setModalProps] = useState({
+    title: "MODAL",
+    onConfirm: () => {},
+  });
+  
+  //const rows = nfts.slice(nfts.length /2);
   const fetchMarketNFTs = async () => {
     const _nfts = await fetchCardsOf(MARKET_CONTRACT_ADDRESS);
     setNFTs(_nfts);
   }
-  // fetchMyNFTs
+  
   const fetchMyNFTs = async () => {
+    if(myAddress === DEFAULT_ADDRESS){
+      alert("NO ADDRESS");
+      return;
+    } 
     const _nfts = await fetchCardsOf(myAddress);
     setNFTs(_nfts);
   }
 
-  // onclickMint
   const onClickMint = async (uri) => {
-    if(myAddress === DEFAULT_ADDRESS) alert("NO ADDRESS");
+    if(myAddress === DEFAULT_ADDRESS){
+      alert("NO ADDRESS");
+      return;
+    } 
     const randomTokenId = parseInt(Math.random() * 1000000);
     KlipAPI.mintCardWithURI(myAddress, randomTokenId, uri, setQrvalue, (result) => {
       alert(JSON.stringify(result));
@@ -64,10 +68,22 @@ function App() {
 
   const onClickCard = (id) => {
     if(tab === 'WALLET'){
-      onClickMyCard(id);
+      setModalProps({
+        title: "NFT를 마켓에 올리시겠어요?",
+        onConfirm: () => {
+          onClickMyCard(id);
+        },
+      });
+      setShowModal(true);
     }
     if(tab === 'MARKET'){
-      onClickMarketCard(id);
+      setModalProps({
+        title: "NFT를 구매하시겠어요?",
+        onConfirm: () => {
+          onClickMarketCard(id);
+        },
+      });
+      setShowModal(true);
     }
   };
 
@@ -84,12 +100,23 @@ function App() {
   };
 
   const getUserData = () => {
-    KlipAPI.getAddress(setQrvalue, async (address) => {
-      setMyAddress(address);
-      const _balance = await getBalance(address);
-      setMyBalance(_balance);
+    setModalProps({
+      title: "Klip 지갑을 연동하시겠습니까?",
+      onConfirm: () => {
+        KlipAPI.getAddress(setQrvalue, async (address) => {
+          setMyAddress(address);
+          const _balance = await getBalance(address);
+          setMyBalance(_balance);
+        });    
+      },
     });
+    setShowModal(true);
   };
+
+  useEffect(() => {
+    getUserData();
+    fetchMarketNFTs();
+  },[])
 
   return (
     <div className="App">
@@ -114,29 +141,52 @@ function App() {
           >
             {myBalance}
           </Alert>
-          <Container 
-            style={{
-              backgroundColor:"white",
-              width: 300,
-              height: 300,
-              padding: 20,
-            }}
-          >   
-          <QRcode value={qrvalue} size={256} style={{margin: "auto"}}/> 
-          </Container>
+          
+          {qrvalue !== "DEFAULT" ? (
+            <Container 
+              style={{
+                backgroundColor:"white",
+                width: 300,
+                height: 300,
+                padding: 20,
+              }}
+            >
+            <QRcode value={qrvalue} size={256} style={{margin: "auto"}}/> 
+            </Container>
+          ) : null}
+          
           {/* 갤러리(마켓, 내 지갑) */}
           {tab === "MARKET" || tab === "WALLET" ? (
             <div className="container" style={{padding:0, width:"100%"}}>
-              {nfts.map((nft, index) => (
-                <Card.Img 
-                  key={`imagekey${index}`}
-                  onClick={()=>{
-                  onClickCard(nft.id);
-                }}
-                className="img-responsive"
-                 src={nfts[index].uri} 
-                />
-              ))}
+              {/*rows.map((o, rowIndex) => {
+                <Row>
+                  <Col 
+                    style={{
+                      marginRigh: 0,
+                      padddingRight: 0
+                    }}
+                  >
+                    <Card 
+                      onClick={() =>{
+                        onClickCard(nfts[rowIndex * 2].id);
+                    }}>
+                      <Card.Img src={nfts[rowIndex * 2].uri}/>
+                    </Card>
+                    {nfts[rowIndex * 2].id}NFT
+                  </Col>
+                </Row>
+              })*/}
+                {nfts.map((nft, index) =>{
+                  <Card.Img
+                    key={`imagekey${index}`}
+                    onClick={() => {
+                      onClickCard(nft.id);
+                    }}
+                    classNAme="img-responsive"
+                    src={nfts[index].uri}
+                  />
+                })}
+              
           </div>
           ) : null}
           {/* 발행 페이지 */}
@@ -181,6 +231,53 @@ function App() {
 
 
         {/* 모달 */}
+        <Modal
+          centered
+          size="sm"
+          show={showModal}
+          onHide={() => {
+            setShowModal(false);
+          }}
+        >
+          <Modal.Header 
+            style={{
+              border: 0,
+              backgroundColor: "black",
+              opacity: 0.8
+            }}
+          >
+            <Modal.Title>{modalProps.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Footer 
+            style={{
+              border: 0,
+              backgroundColor: "black",
+              opacity: 0.8
+            }}
+          >
+            <Button 
+              variant="secondary" 
+              onClick={() => {
+                setShowModal(false);
+              }}
+            >
+              닫기
+            </Button>
+            <Button
+              variant="primary"
+              onClick={() => {
+                modalProps.onConfirm();
+                setShowModal(false);
+              }}
+              style={{
+                backgroundColor: "#810034",
+                borderColor: "#810034"
+              }}
+            >
+              진행
+            </Button>
+          </Modal.Footer>
+        </Modal>
         {/* 탭 */}
         <nav style={{backgroundColor: "#1b1717", height: 45 }} className="navbar fixed-bottom navbar-light" role="navigation">
           <Nav className="w-100">
